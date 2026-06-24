5f9ebe0601bd79f915936450f65552d8a1f26102
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2026-06-24 11:42:27 +0600

    style added

--- WebApplicationTutorial1/WebApplicationTutorial1/Program.cs
+++ WebApplicationTutorial1/WebApplicationTutorial1/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-//app.UseRouting();
+app.UseStaticFiles();
 app.MapControllers();
 app.Run();
